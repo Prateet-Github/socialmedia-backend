bb4b86db6d7031ed3559b733b620745f548c1e51
Prateet Tiwari <prateettiwari29@gmail.com>
2025-12-01 03:39:23 +0530

    feat: add user profile endpoint to retrieve user information

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -60,3 +60,16 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/user.controller.js';
+import { registerUser, loginUser, getUserProfile } from '../controllers/user.controller.js';
+import  protect  from '../middlewares/user.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
 
 export default router;
